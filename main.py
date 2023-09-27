cdc21ca9d071d4f171b80ef873655226e3a56a43
Kajetan Knopp <kajetan@knopp.com.pl>
2023-09-27 14:01:56 +0200

    Changed Flask routing to reflect our solution

--- main.py
+++ main.py
@@ -2,34 +2,52 @@
 
 # Main flask imports
 from flask import Flask, render_template, redirect, request, url_for, session, flash
-# Database import
 from flask_sqlalchemy import SQLAlchemy
-# Form import
-from wtforms import Form, StringField, IntegerField, validators
+import datetime
+
+# Training import
+from training import *
 
 # Main flask definitions
 app = Flask(__name__)
-app.secret_key = "Code4Ukraine"
+app.secret_key = "squishstick"
 app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///database.db'
 
 # Database definition
 db = SQLAlchemy(app)
 
-class Person(db.Model):
+class Sessions(db.Model):
     Id = db.Column(db.Integer, primary_key=True)
-    Age = db.Column(db.Integer, nullable=False)
-    Name = db.Column(db.String(50), nullable=False)
+    StartDate = db.Column(db.DateTime, nullable=False)
+    EndDate = db.Column(db.DateTime, nullable=False)
+    Average = db.Column(db.Float, nullable=False)
+    Average_F1 = db.Column(db.Float, nullable=False)
+    Average_F2 = db.Column(db.Float, nullable=False)
+    Average_F3 = db.Column(db.Float, nullable=False)
+    Average_F4 = db.Column(db.Float, nullable=False)
+    Average_P = db.Column(db.Float, nullable=False)
 
     def __repr__(self):
-        return f"Person: {self.Id}, Age: {self.Age}, Name: {self.Name}"
+        return f"Session: {self.Id}, Date: {self.Date}, Score: {self.Average}"
+    
+class Measurements(db.Model):
+    Id = db.Column(db.Integer, primary_key=True)
+    Session_Id = db.Column(db.Integer, db.ForeignKey('sessions.Id'), nullable=False)
+    Date = db.Column(db.DateTime, nullable=False, default=datetime.datetime.utcnow())
+    F1 = db.Column(db.Float, nullable=False)
+    F2 = db.Column(db.Float, nullable=False)
+    F3 = db.Column(db.Float, nullable=False)
+    F4 = db.Column(db.Float, nullable=False)
+    P = db.Column(db.Float, nullable=False)
+
+    def __repr__(self):
+        return f"Measurement: {self.Id}, Session: {self.Session_Id}, Date: {self.Date}, F1: {self.F1}, F2: {self.F2}, F3: {self.F3}, F4: {self.F4}, P: {self.P}"
+
+app.app_context().push()
+
 
 db.create_all()
 
-# Form definition
-
-class PersonForm(Form):
-    name = StringField('Name', [validators.Length(min=4, max=25)])
-    age = IntegerField('Age', [validators.NumberRange(min=0, max=140)])
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
@@ -42,38 +60,16 @@ def main():
             return redirect(url_for("last"))
     return render_template('index.html')
 
-@app.route('/add', methods=['GET', 'POST'])
-def add():
-    form = PersonForm(request.form) 
-    if request.method == "POST" and form.validate():
-        person = Person(Name=form.name.data, Age=form.age.data)
-        db.session.add(person)
-        db.session.commit()
-        # Adding this person as last one added
-        session["last"] = person.Id
-        flash("Thanks for adding a new person to the database!")
-        return redirect(url_for('main'))
-    else:
-        return render_template("add.html", form=form)
+@app.route('/current')
+def current():
+    session = Sessions.query.order_by(Sessions.Id.desc()).first()
+    measurements = Measurements.query.filter_by(Session_Id=session.Id).all()
+    return render_template('current.html', session = session, measurements = measurements)
 
-@app.route('/all')
-def all():
-    people = Person.query.all()
-    return render_template('all.html', people = people)
-
-@app.route('/one/<int:id>')
-def one(id):
-    person = Person.query.filter_by(Id=id).first()
-    return render_template('one.html', person = person)
-
-@app.route('/last')
-def last():
-    if('last' not in session):
-        flash("No people added in this session!")
-        return redirect(url_for("main"))
-    else:
-        last_person = session["last"]
-        return redirect(url_for("one", id=last_person))
+@app.route('/progress')
+def progress():
+    sessions = Sessions.query.all()
+    return render_template('progress.html', sessions = sessions)
 
 
 if __name__ == '__main__':
